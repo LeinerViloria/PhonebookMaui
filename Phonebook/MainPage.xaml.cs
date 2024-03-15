c68ceb704d7dfc6de7321cd30c28bd72c15ad28a
LeinerViloria <lvilora@siesa.com>
2024-03-14 21:15:50 -0500

    Ajustes

--- Phonebook/MainPage.xaml.cs
+++ Phonebook/MainPage.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace Phonebook
+﻿using Phonebook.Models;
+using System.Collections.ObjectModel;
+
+namespace Phonebook
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<ContactDTO> Contacts { get; set; }
         public MainPage()
         {
             InitializeComponent();
+
+            Contacts = new ObservableCollection<ContactDTO>(App.Contacts);
+
+            BindingContext = this;
         }
     }
 
